4de190aa9838a41b6053c9a0f354731f6cc8e4c5
Ivan <ivanpetrlikazabache@gmail.com>
2020-04-15 23:52:44 -0500

    Primer avance del Codigo Fuente
    Este es  el  primer  avance del  codigo  fuente de un  proyecto en Java

---
+++ src/Respositorio/Programa01.java
@@ -0,0 +1,14 @@
+
+package Respositorio;
+
+
+public class Programa01 
+{
+    public static void main(String[] args)
+    {
+       
+        System.out.println("Hola Mundo Cruel !!!!!!");   
+        
+    }
+    
+}
